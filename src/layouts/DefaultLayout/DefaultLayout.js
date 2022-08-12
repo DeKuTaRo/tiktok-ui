1c120ffa19d78e526184fae3bf73866873626f1b
DeKuTaRo <thanhtuan09102001@gmail.com>
2022-08-12 23:20:24 +0700

    Xây dựng UI phần sidebar #1

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
 import Header from '~/layouts/components/Header';
-import Sidebar from './Sidebar';
+
 import styles from './DefaultLayout.module.scss';
 import classNames from 'classnames/bind';
+import Sidebar from '~/layouts/components/Sidebar';
 
 const cx = classNames.bind(styles);
 
